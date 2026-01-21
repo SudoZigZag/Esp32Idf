a45727bb74cf028b1680e4266dd53e93e7abcb08
Ravi Iyer <you@example.com>
2026-01-20 22:56:44 -0800

    Add WiFi, mDNS, and multi-threading support

---
+++ main/wifi_passwords_sample.h
@@ -0,0 +1,6 @@
+// ========================================
+// WiFi Configuration - CHANGE THESE and rename the file to wifi_passwords.h
+// ========================================
+#define WIFI_SSID      // Your WiFi network name
+#define WIFI_PASSWORD  //Your WiFi password
+#define MDNS_HOSTNAME  "espXY"
